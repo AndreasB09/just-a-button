f03e4aefbaea1bb0a6c5122e274f3601374af720
AndreasB09 <andreasb09.kodehode@gmail.com>
2024-07-20 23:54:40 +0200

    spinning circle chaos

--- app.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('.circle-btn').addEventListener('click', () => {
-    const innerHalfCircle = document.querySelector('.circle-inner');
-    const outerHalfCircle = document.querySelector('.circle-outer');
-
-    leftHalfCircle.style.transform = leftHalfCircle.style.transform === 'rotate(360deg)' ? 'rotate(0deg)' : 'rotate(360deg)';
-    rightHalfCircle.style.transform = rightHalfCircle.style.transform === 'rotate(-360deg)' ? 'rotate(0deg)' : 'rotate(-360deg)';
-});
